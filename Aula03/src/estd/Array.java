c0d546cdde451ef7b84168bf003230df57cd05de
Felipe <Felipe@LAPTOP-TH289NJC>
2017-03-06 20:34:19 -0300

    Aula03

---
+++ Aula03/src/estd/Array.java
@@ -0,0 +1,14 @@
+package estd;
+
+import java.util.Arrays;
+
+public class Array {
+	
+	int[] i = {5,6,7}; //O Array é um objeto
+	Integer[] y;
+	
+	public void test(){
+		System.out.println(Arrays.toString(i));	
+	}
+	
+}
